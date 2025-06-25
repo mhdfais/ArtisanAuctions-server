b098a7f630debe567abfa550492b311e200a229d
Mohammed Fais TM <mhdfaistm@gmail.com>
2025-06-25 09:27:54 +0530

    session debuggiing

--- src/app.ts
+++ src/app.ts
@@ -18,6 +18,8 @@ if (!sessionSecret) {
 }
 const app = express();
 
+app.set('trust proxy', 1);
+
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
